bc4c67a56236ba22fc95a998fc163fd3dbd07412
distalx <distalx@gmail.com>
2017-11-08 19:56:41 +0530

    update test

--- src/uhr.test.js
+++ src/uhr.test.js
@@ -7,17 +7,17 @@ describe('uhr',function(){
     const flag = 1;
     const result = flag * new Date().getTimezoneOffset() / 60;
 
-    it('runs successfully',function(){
+    it('return current timezone offset in hour',function(){
       expect(uhr.get_current_timezone_offset_in_hours(flag)).to.equal(result);
     });
 
-    it('missing flag value',function(){
+    it('throws an err missing flag value',function(){
       expect(function(){
           uhr.get_current_timezone_offset_in_hours()
       }).to.throw('missing flag value');
     });
 
-    it('invalid flag value',function(){
+    it('throws an err invalid flag value',function(){
       expect(function(){
           uhr.get_current_timezone_offset_in_hours(4)
       }).to.throw('invalid flag value');
@@ -27,7 +27,7 @@ describe('uhr',function(){
 
   describe('get_offset_minute',function(){
 
-    it('runs successfully',function(){
+    it('return offset minute',function(){
       expect(uhr.get_offset_minute(5.5)).to.equal(30);
     });
 
@@ -35,7 +35,7 @@ describe('uhr',function(){
 
   describe('get_offset_hour',function(){
 
-    it('runs successfully',function(){
+    it('return offset hour',function(){
       expect(uhr.get_offset_hour(5.5)).to.equal(5);
     });
 
@@ -43,13 +43,13 @@ describe('uhr',function(){
 
   describe('get_new_minute',function(){
 
-    it('runs successfully',function(){
+    it('return new minute',function(){
       expect(uhr.get_new_minute(30, 10)).to.equal(40);
     });
-    it('runs successfully',function(){
+    it('return new minute',function(){
       expect(uhr.get_new_minute(30, 40)).to.equal(10);
     });
-    it('runs successfully',function(){
+    it('return new minute',function(){
       expect(uhr.get_new_minute(30, -40)).to.equal(50);
     });
 
@@ -57,15 +57,15 @@ describe('uhr',function(){
 
   describe('get_carry_hour',function(){
 
-    it('runs successfully',function(){
+    it('return 0 carry hour',function(){
       expect(uhr.get_carry_hour(30, 10)).to.equal(0);
     });
 
-    it('runs successfully',function(){
+    it('return 1 carry hour',function(){
       expect(uhr.get_carry_hour(30, 40)).to.equal(1);
     });
 
-    it('runs successfully',function(){
+    it('return -1 carry hour',function(){
       expect(uhr.get_carry_hour(30, - 40)).to.equal(-1);
     });
 
@@ -73,15 +73,15 @@ describe('uhr',function(){
 
   describe('get_new_hour',function(){
 
-    it('runs successfully',function(){
+    it('return new hour',function(){
       expect(uhr.get_new_hour(5, 1, 0)).to.equal(6);
     });
 
-    it('runs successfully',function(){
+    it('return new hour plus a day',function(){
       expect(uhr.get_new_hour(5, -6, 0)).to.equal(23);
     });
 
-    it('runs successfully',function(){
+    it('return new hour minues a day',function(){
       expect(uhr.get_new_hour(22, 1, 1)).to.equal(0);
     });
 
@@ -89,15 +89,15 @@ describe('uhr',function(){
 
   describe('get_carry_day',function(){
 
-    it('runs successfully',function(){
+    it('return zero carry day',function(){
       expect(uhr.get_carry_day(5, 1, 0)).to.equal(0);
     });
 
-    it('runs successfully',function(){
+    it('return -1 carry day',function(){
       expect(uhr.get_carry_day(5, -6, 0)).to.equal(-1);
     });
 
-    it('runs successfully',function(){
+    it('return 1 carry day',function(){
       expect(uhr.get_carry_day(22, 1, 1)).to.equal(1);
     });
 
@@ -105,15 +105,15 @@ describe('uhr',function(){
 
   describe('get_new_day',function(){
 
-    it('runs successfully',function(){
+    it('return a new day',function(){
       expect(uhr.get_new_day(1, 0)).to.equal(1);
     });
 
-    it('runs successfully',function(){
+    it('return a new day minues a week',function(){
       expect(uhr.get_new_day(1, 6)).to.equal(0);
     });
 
-    it('runs successfully',function(){
+    it('return a new day plus a week',function(){
       expect(uhr.get_new_day(1, -2)).to.equal(6);
     });
 
